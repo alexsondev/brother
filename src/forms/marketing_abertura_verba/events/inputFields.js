d5ff3791437dd08c617301f10a4eec974f54d45b
Alexson Ferreira <dev@alexson.dev>
2024-02-29 17:26:27 -0300

    Update inputFields.js and npm-shrinkwrap.json

--- src/forms/marketing_abertura_verba/events/inputFields.js
+++ src/forms/marketing_abertura_verba/events/inputFields.js
@@ -85,10 +85,10 @@ function inputFields(form) {
 
 
     // se formato da data for 99/99/9999, converter para timestamp
-    if (inicioAcao?.indexOf('/') > -1) {
+    if (String(inicioAcao)?.indexOf('/') > -1) {
       form.setValue('inicioAcao', new Date(inicioAcao).getTime());
     }
-    if (terminoAcao?.indexOf('/') > -1) {
+    if (String(terminoAcao)?.indexOf('/') > -1) {
       form.setValue('terminoAcao', new Date(terminoAcao).getTime());
     }
 
@@ -96,7 +96,7 @@ function inputFields(form) {
     [
       { table: "itensSellout", child: "itemSellout" },
       { table: "itensSellinIt", child: "itemSellinIt" },
-      { table: "itensprice", child: "itemprice" },
+      { table: "itensPrice", child: "itemPrice" },
       { table: "itensSpiffIt", child: "itemSpiffIt" },
     ].forEach(({ table, child }) => {
 
